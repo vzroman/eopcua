f27f400f1ca05df2f3e38d7d6bb2b15577f25489
roman <vzroman@faceplate.io>
2021-04-27 17:26:22 +0600

    basedir fixing

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -7,7 +7,7 @@ set -e
 
 # the script folder
 DIR=$PWD
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # dive into c_src
 cd $BASEDIR
